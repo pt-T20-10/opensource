f3f89d52d6573482903939809263817eb816f417
TrungNghia1409 <trungnghiahb1409@gmail.com>
2025-10-23 15:35:47 +0700

    Cau 7, Cau 8

---
+++ bai7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int GiaiThua(int n){
+    if (n==0 || n ==1)
+        return 1;
+    else    
+        return n * GiaiThua(n - 1);
+}
+
+void InGiaithua(){
+
+    int n, kq;
+    printf("Nhap N!: ");
+    scanf("%d", &n);
+    kq = GiaiThua(n);
+    printf("%d! = %d\n", n, kq);
+}
+
