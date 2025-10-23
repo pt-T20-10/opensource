c6b325074f48431c694a0be0bd51597c1c18ad83
nhnghia214 <nhoangnghia2104@gmail.com>
2025-10-23 15:30:36 +0700

    xong bai 9 + 10 + 11

---
+++ bai10.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int Fibonacci(int m){
+    if (m == 0)
+        return 0;
+    if (m == 1 || m == 2)
+        return 1;
+    else
+        return Fibonacci(m-1) + Fibonacci(m-2);
+}
+
+void InDayFibonacci() {
+    int m, n, i;
+
+    printf("Nhap so luong phan tu muon in (m): ");
+    scanf("%d", &m);
+    printf("Nhap gioi han (n): ");
+    scanf("%d", &n);
+
+    printf("\nDay Fibonacci:\n");
+    for (i = 0; i <= m; i++) {
+        int f = Fibonacci(i);
+        if (f >= n)
+            break;
+        printf("F[%d] = %d\n", i, f);
+        
+    }
+}
+
+int main(){
+    InDayFibonacci();
+    return 0;
+}
+
+
