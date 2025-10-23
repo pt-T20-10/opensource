a80d52e8a01d0f5c93e291ce9fcfb7b95bc7a914
Phan Trọng Thoại <138378089+pt-T20-10@users.noreply.github.com>
2025-10-23 15:22:39 +0700

    Chinh main

--- main.c
+++ main.c
@@ -4,6 +4,15 @@ void menu(){
     printf("*** MENU ***\n");
     printf("1- Xem ngay cua thang\n");
     printf("2- Kiem tra nam nhuan\n");
+    printf("3- Tinh phuong trinh bac hai\n");
+    printf("4- Tinh diem trung binh ba mon\n");
+    printf("5- Tinh tien dien\n");
+    printf("6- In bang cuu chuong\n");
+    printf("7- Tinh giai thua\n");
+    printf("8- In tam giac vuong\n");
+    printf("9- Kiem tra nhap so tuan tu\n");
+    printf("10- In day fibonnaci nho hon n\n");
+    printf("11- In tam giac can\n");
     printf("------------------\n");
     printf("chon STT chuc nang: \n");
     printf("------------------\n");
@@ -20,7 +29,42 @@ void menu(){
         namnhuan();
         menu();
         break;
-    
+    case 3:
+        xemthang();
+        menu();
+        break;
+    case 4:
+        namnhuan();
+        menu();
+        break;
+    case 5:
+        xemthang();
+        menu();
+        break;
+    case 6:
+        namnhuan();
+        menu();
+        break;
+    case 7:
+        xemthang();
+        menu();
+        break;
+    case 8:
+        namnhuan();
+        menu();
+        break;
+    case 9:
+        xemthang();
+        menu();
+        break;
+    case 10:
+        namnhuan();
+        menu();
+        break;
+    case 11:
+        xemthang();
+        menu();
+        break; 
     default:
         menu();
         break;
